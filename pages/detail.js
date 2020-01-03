fe45a2b93005fbca51fe29a88796202f1ebad1a1
wencaizhang <1052642137@qq.com>
2020-01-03 15:51:38 +0800

    增加markdown渲染和navbar导航

--- pages/detail.js
+++ pages/detail.js
@@ -1,11 +1,50 @@
 import React,{useState} from 'react'
 import Head from 'next/head'
-import { Row, Col , Icon ,Breadcrumb } from 'antd'
+import { Row, Col, Icon ,Breadcrumb, Affix } from 'antd'
+
+import ReactMarkdown from 'react-markdown'
+import MarkNav from 'markdown-navbar';
+import 'markdown-navbar/dist/navbar.css';
 
 import Header from '../components/Header'
 import Author from '../components/Author'
 import Footer from '../components/Footer'
 import '../static/style/pages/detail.css'
+
+let markdown='# P01:课程介绍和环境搭建\n' +
+  '[ **M** ] arkdown + E [ **ditor** ] = **Mditor**  \n' +
+  '> Mditor 是一个简洁、易于集成、方便扩展、期望舒服的编写 markdown 的编辑器，仅此而已... \n\n' +
+   '**这是加粗的文字**\n\n' +
+  '*这是倾斜的文字*`\n\n' +
+  '***这是斜体加粗的文字***\n\n' +
+  '~~这是加删除线的文字~~ \n\n'+
+  '\`console.log(111)\` \n\n'+
+  '# p02:来个Hello World 初始Vue3.0\n' +
+  '> aaaaaaaaa\n' +
+  '>> bbbbbbbbb\n' +
+  '>>> cccccccccc\n'+
+  '***\n\n\n' +
+  '# p03:Vue3.0基础知识讲解\n' +
+  '> aaaaaaaaa\n' +
+  '>> bbbbbbbbb\n' +
+  '>>> cccccccccc\n\n'+
+  '# p04:Vue3.0基础知识讲解\n' +
+  '> aaaaaaaaa\n' +
+  '>> bbbbbbbbb\n' +
+  '>>> cccccccccc\n\n'+
+  '#5 p05:Vue3.0基础知识讲解\n' +
+  '> aaaaaaaaa\n' +
+  '>> bbbbbbbbb\n' +
+  '>>> cccccccccc\n\n'+
+  '# p06:Vue3.0基础知识讲解\n' +
+  '> aaaaaaaaa\n' +
+  '>> bbbbbbbbb\n' +
+  '>>> cccccccccc\n\n'+
+  '# p07:Vue3.0基础知识讲解\n' +
+  '> aaaaaaaaa\n' +
+  '>> bbbbbbbbb\n' +
+  '>>> cccccccccc\n\n'+
+  '``` var a=11; ```'
 
 const Detail = () => (
   <div>
@@ -30,15 +69,16 @@ const Detail = () => (
                 <div className="detailed-title">
                   React实战视频教程
                 </div>
-
                 <div className="list-icon center">
                   <span><Icon type="calendar" /> 2019-06-28</span>
                   <span><Icon type="folder" /> 视频教程</span>
                   <span><Icon type="fire" /> 5498人</span>
                 </div>
-
                 <div className="detailed-content" >
-                  详细内容，下节课编写
+                  <ReactMarkdown 
+                    source={markdown} 
+                    escapeHtml={false}  
+                  />
                 </div>
              </div>
             </div>
@@ -46,6 +86,16 @@ const Detail = () => (
 
         <Col className="comm-right" xs={0} sm={0} md={7} lg={5} xl={4}>
           <Author />
+          <Affix offsetTop={5}>
+            <div className="detailed-nav comm-box">
+              <div className="nav-title">文章目录</div>
+              <MarkNav
+                className="article-menu"
+                source={markdown}
+                ordered={false}
+              />
+            </div>
+          </Affix>
         </Col>
       </Row>
       <Footer/>

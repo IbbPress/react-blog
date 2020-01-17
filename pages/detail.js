a9cb9d875bfb178d7dff71c7a55657f45e1ec882
wencaizhang <1052642137@qq.com>
2020-01-17 10:37:09 +0800

    refactor: render markdown

--- pages/detail.js
+++ pages/detail.js
@@ -4,60 +4,30 @@ import { Row, Col, Icon ,Breadcrumb, Affix } from 'antd'
 import axios from 'axios'
 import dayjs from 'dayjs'
 
-// import ReactMarkdown from 'react-markdown'
-import MarkNav from 'markdown-navbar';
-import 'markdown-navbar/dist/navbar.css';
-
 import marked from 'marked'
-// import hljs from "highlight.";
 import hljs from "highlight.js"
-
 import 'highlight.js/styles/monokai-sublime.css';
+import Tocify from '../components/tocify.tsx'
 
 import Header from '../components/Header'
 import Author from '../components/Author'
 import Footer from '../components/Footer'
 import '../static/style/pages/detail.css'
 
-let markdown='# P01:课程介绍和环境搭建\n' +
-  '[ **M** ] arkdown + E [ **ditor** ] = **Mditor**  \n' +
-  '> Mditor 是一个简洁、易于集成、方便扩展、期望舒服的编写 markdown 的编辑器，仅此而已... \n\n' +
-   '**这是加粗的文字**\n\n' +
-  '*这是倾斜的文字*`\n\n' +
-  '***这是斜体加粗的文字***\n\n' +
-  '~~这是加删除线的文字~~ \n\n'+
-  '\`console.log(111)\` \n\n'+
-  '# p02:来个Hello World 初始Vue3.0\n' +
-  '> aaaaaaaaa\n' +
-  '>> bbbbbbbbb\n' +
-  '>>> cccccccccc\n'+
-  '***\n\n\n' +
-  '# p03:Vue3.0基础知识讲解\n' +
-  '> aaaaaaaaa\n' +
-  '>> bbbbbbbbb\n' +
-  '>>> cccccccccc\n\n'+
-  '# p04:Vue3.0基础知识讲解\n' +
-  '> aaaaaaaaa\n' +
-  '>> bbbbbbbbb\n' +
-  '>>> cccccccccc\n\n'+
-  '#5 p05:Vue3.0基础知识讲解\n' +
-  '> aaaaaaaaa\n' +
-  '>> bbbbbbbbb\n' +
-  '>>> cccccccccc\n\n'+
-  '# p06:Vue3.0基础知识讲解\n' +
-  '> aaaaaaaaa\n' +
-  '>> bbbbbbbbb\n' +
-  '>>> cccccccccc\n\n'+
-  '# p07:Vue3.0基础知识讲解\n' +
-  '> aaaaaaaaa\n' +
-  '>> bbbbbbbbb\n' +
-  '>>> cccccccccc\n\n'+
-  '``` var a=11; ```'
+const Detail = (props) =>{
+
+  const [ tocify,setTocify ] = useState(new Tocify())
+  const [ post ] = useState(props.data)
+  const [ html, setHtml ] = useState(props.data.content)
+  const [ loading, setLoading ] = useState(false)
 
   const renderer = new marked.Renderer();
-
+  renderer.heading = function(text, level, raw) {
+    const anchor = tocify.add(text, level);
+    return `<a id="${anchor}" href="#${anchor}" class="anchor-fix"><h${level}>${text}</h${level}></a>\n`;
+  };
   marked.setOptions({
-    renderer: renderer, 
+    renderer: renderer,
     gfm: true,
     pedantic: false,
     sanitize: false,
@@ -65,19 +35,10 @@ let markdown='# P01:课程介绍和环境搭建\n' +
     breaks: false,
     smartLists: true,
     smartypants: false,
-    highlight (code) {
+    highlight: function (code) {
       return hljs.highlightAuto(code).value;
-    },
-  }); 
-  
-      // let html = marked(props.article_content) 
-      
-const Detail = (props) =>{
-  console.log('props: ', props.data);
-
-  const [ post ] = useState(props.data)
-  const [ html, setHtml ] = useState(props.data.content)
-  const [ loading, setLoading ] = useState(false)
+    }
+  });
 
   const myFuction = async () => {
     let newhtml =await marked(post.content)
@@ -131,11 +92,7 @@ const Detail = (props) =>{
             <Affix offsetTop={5}>
               <div className="detailed-nav comm-box">
                 <div className="nav-title">文章目录</div>
-                <MarkNav
-                  className="article-menu"
-                  source={post.content}
-                  ordered={false}
-                />
+                {tocify && tocify.render()}
               </div>
             </Affix>
           </Col>
